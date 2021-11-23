7071033b832fa12b0420e3d920c04941f3b1ff6c
TothGergelyGy <50365016+TothGergelyGy@users.noreply.github.com>
2021-11-23 16:05:14 +0100

    ==

--- Modbus_Master_v2.py
+++ Modbus_Master_v2.py
@@ -20,20 +20,20 @@ for i in data['clients']:
 while True:
     for i in data['commands']:
         if i['type']=="coil" :
-            if i['action']="read_and_write":
+            if i['action']=="read_and_write":
                 x=data['clients'][i['from']]['modbusclient'].read_coils(i['readaddress'],1)
                 print("read: "+i['type']+" from: "+i['from']+" address: "+i['readaddress']+" value: "+str(x))
                 data['clients'][i['to']]['modbusclient'].write_multiple_coils(x,1)
-            elif i['action']="read":
+            elif i['action']=="read":
                 x=data['clients'][i['from']]['modbusclient'].read_coils(i['readaddress'],1)
                 print("read: "+i['type']+" from: "+i['from']+" address: "+i['readaddress']+" value: "+str(x))
         elif i['type']=="reg":
-            if i['action']="read_and_write":
+            if i['action']=="read_and_write":
                 x=data['clients'][i['from']]['modbusclient'].read_holding_register(i['readaddress'],1)
                 print("read: "+i['type']+" from: "+i['from']+" address: "+i['readaddress']+" value: "+str(x))
                 data['clients'][i['to']]['modbusclient'].write_multiple_registers(x,1)
                
-            elif i['action']="read":
+            elif i['action']=="read":
                 x=data['clients'][i['from']]['modbusclient'].read_holding_register(i['readaddress'],1)
                 print("read: "+i['type']+" from: "+i['from']+" address: "+i['readaddress']+" value: "+str(x))
 f.close()
